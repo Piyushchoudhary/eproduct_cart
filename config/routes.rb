200189cb1c3a191ae4769008d75cef7fc55136cd
spatle-drg <spatle@dresources.com>
2017-02-22 00:50:52 +0530

    Task-4: 1. Add category views 2. Add admin/categories resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  namespace :admin do
+    resources :categories
+  end
   #devise_for :users
   devise_for :users, controllers: {
       sessions: 'users/sessions',
